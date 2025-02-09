8a97b60bf0c6757e947f500a5b569d549967a507
Helmare <h3lmare@gmail.com>
2025-02-09 01:14:28 -0600

    Basic functionallity on a test channel.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Snapcord
 
-Turns any text channel into a Snapchat-style chat, where messages are deleted after 24 hours. You can save messages by reacting with the 💾 emoji.
+Turns text channels into a Snapchat-style chats, where messages are deleted after 24 hours. You can save messages by reacting with 💾.
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { Client, IntentsBitField } from 'discord.js';
+import {
+  Client,
+  IntentsBitField,
+  Message,
+  SlashCommandBuilder,
+} from 'discord.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -12,8 +17,49 @@ const client = new Client({
   ],
 });
 
-client.on('ready', (client0) => {
-  console.log(`${client0.user.tag} is online`);
-});
+client.on('ready', async () => {
+  console.log(`${client.user.tag} is online 🎉`);
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+  const channel = await client.channels.fetch(process.env.DEV_CHANNEL_ID);
+  setInterval(async () => {
+    const cutoff = Date.now() - process.env.MESSAGE_MAX_AGE;
+
+    /** @type {Message[]} */
+    const messages = await channel.messages.fetch({ limit: 100 });
+    messages.forEach(async (message) => {
+      // Ignore messages whether the auther is this bot.
+      if (message.author.id == process.env.DISCORD_CLIENT_ID) return;
+
+      // Ignore messages with the :floppy_disk: reaction.
+      if (await _hasReaction(message, '💾')) return;
+
+      // Ignore messages that are young.
+      if (message.createdTimestamp > cutoff) return;
+
+      // Delete the rest of the messages.
+      message
+        .delete()
+        .then(() => console.log(`Deleted message {${message.id}}`))
+        .catch((e) => {
+          if (e.status != 404) console.log(e);
+        });
+    });
+  }, process.env.APP_INTERVAL);
+});
+/**
+ * Gets whether or not a message has a reaction.
+ *
+ * @param {Message} message
+ * @param {string} key
+ * @return {Promise<boolean>}
+ */
+async function _hasReaction(message, key) {
+  const reaction = message.reactions.cache.get(key);
+  try {
+    return reaction && (await reaction.users.fetch()).size > 0;
+  } catch (e) {
+    return false;
+  }
+}
+
+client.login(process.env.DISCORD_CLIENT_TOKEN);
