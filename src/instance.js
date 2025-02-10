ebcc1e168c6b1d5978d3bcd118acfbafe7b79487
Helmare <h3lmare@gmail.com>
2025-02-10 08:43:02 -0600

    Implemented CRUD functions.

--- src/instance.js
+++ src/instance.js
@@ -1,4 +1,7 @@
 import { neon } from '@neondatabase/serverless';
+import pino from 'pino';
+
+const logger = pino();
 
 /**
  * @typedef {object} Instance
@@ -21,6 +24,10 @@ export class InstanceRepository {
    */
   async fetch() {
     const results = await this.sql`SELECT * FROM instances;`;
+    logger.info(
+      `fetched ${results.length} instance${results.length == 1 ? '' : 's'}`
+    );
+
     this.instances = results;
     return results;
   }
@@ -31,5 +38,56 @@ export class InstanceRepository {
   async all() {
     if (this.instances.length === 0) await this.fetch();
     return this.instances;
+  }
+
+  /**
+   * Gets an instance by channel id.
+   * @param {string} channelId
+   * @return {Promise<Instance>}
+   */
+  async getByChannelId(channelId) {
+    const results = await this
+      .sql`SELECT * FROM instances WHERE channel_id=${channelId};`;
+
+    if (results.length === 0) {
+      return undefined;
+    } else {
+      return results[0];
+    }
+  }
+
+  /**
+   * Creates a new instance by channel id.
+   * @param {string} channelId
+   * @param {number} maxMessageAge
+   * @return {Promise<Instance|undefined>}
+   */
+  async create(channelId, maxMessageAge) {
+    try {
+      const results = await this
+        .sql`INSERT INTO instances (channel_id, max_message_age) VALUES (${channelId}, ${maxMessageAge}) RETURNING *;`;
+      logger.info(results[0], 'created new instance');
+      return results[0];
+    } catch (err) {
+      logger.error(err, 'failed to create instance');
+      return undefined;
+    }
+  }
+
+  /**
+   * Delete instance by id.
+   * @param {number} id
+   * @return {Promise<boolean>}
+   */
+  async delete(id) {
+    try {
+      await this.sql`DELETE FROM instances WHERE id=${id};`;
+      this.instances = this.instances.filter((i) => i.id != id);
+      logger.info(`deleted instance ${id}`);
+      return true;
+    } catch (err) {
+      logger.error(err, 'failed to delete instance');
+      return false;
+    }
   }
 }
